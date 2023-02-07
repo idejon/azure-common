ee5309bbafca07173ab9d096b5c60b08ad341db3
Jon Ide <jon@4900n.com>
2023-02-07 15:04:50 -0600

    add vnet

--- azure-common/main.tf
+++ azure-common/main.tf
@@ -18,3 +18,23 @@ resource "azurerm_resource_group" "common" {
     name     = "common-${var.location}-rg"
     location = var.location
 }
+
+resource "azurerm_network_security_group" "hub" {
+  name                = "hub-${var.location}-nsg"
+  location            = azurerm_resource_group.common.location
+  resource_group_name = azurerm_resource_group.common.name
+}
+
+resource "azurerm_virtual_network" "hub" {
+  name                = "hub-${var.location}-network"
+  location            = azurerm_resource_group.common.location
+  resource_group_name = azurerm_resource_group.common.name
+  address_space       = ["10.200.0.0/16"]
+
+  subnet {
+    name           = "hub-${var.location}-subnet"
+    address_prefix = "10.200.0.0/24"
+    security_group = azurerm_network_security_group.common
+  }
+
+}
--- azure-common/outputs.tf
+++ azure-common/outputs.tf
@@ -1,3 +1,38 @@
+## Resource Group
 output "azurerm_resource_group_name" {
   value = azurerm_resource_group.common.name
+}
+
+output "azurerm_resource_group_location" {
+  value = azurerm_resource_group.common.location
+}
+
+output "azurerm_resource_group_id" {
+  value = azurerm_resource_group.common.id
+}
+
+## NSG
+output "azurerm_network_security_group_id" {
+  value = azurerm_network_security_group.hub.id
+}
+
+## VNET
+output "azurerm_virtual_network_name" {
+  value = azurerm_virtual_network.hub.name
+}
+
+output "azurerm_virtual_network_id" {
+  value = azurerm_virtual_network.hub.id
+}
+
+output "azurerm_virtual_network_guid" {
+  value = azurerm_virtual_network.hub.guid
+}
+
+output "azurerm_virtual_network_address_space" {
+  value = azurerm_virtual_network.hub.address_space
+}
+
+output "azurerm_virtual_network_subnet_hub_id" {
+  value = azurerm_virtual_network.hub.subnet.*.id[0]
 }
